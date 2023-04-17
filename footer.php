c0e5c5c74f71d134974d4792c1821d4ddcd2934f
Udit Malik <udit@Udits-MacBook-Air.local>
2023-04-17 03:55:14 -0500

    some

---
+++ footer.php
@@ -0,0 +1,43 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Quick Links</h3>
+         <a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra Links</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us</h3>
+         <p> <i class="fas fa-phone"></i> 615-602-9500 </p>
+         <p> <i class="fas fa-phone"></i> 615-330-8188 </p>
+         <p> <i class="fas fa-envelope"></i> udit.malik@vanderbilt.edu </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Nashville, TN </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> Twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> LinkedIn </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; Made with love in
+      <?php echo date('Y'); ?> at <span>Vanderbilt University.</span>
+   </p>
+
+</section>
