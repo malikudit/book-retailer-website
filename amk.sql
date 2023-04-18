3b6e7c9722fc873635075799fa1039a47e9f56d7
Udit Malik <udit@Udits-MacBook-Air.local>
2023-04-17 21:59:12 -0500

    fixing db

--- amk.sql
+++ amk.sql
@@ -1,32 +1,34 @@
-SET
-  SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 
 START TRANSACTION;
 
-SET
-  time_zone = "+00:00";
+SET time_zone = "+00:00";
 
 CREATE TABLE `cart` (
-  `id` int(100) NOT NULL,
+  `id` int(100) NOT NULL AUTO_INCREMENT,
   `user_id` int(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `author` varchar(100) NOT NULL,
   `price` int(100) NOT NULL,
   `quantity` int(100) NOT NULL,
-  `image` varchar(100) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+  `image` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_cart_users` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `message` (
-  `id` int(100) NOT NULL,
+  `id` int(100) NOT NULL AUTO_INCREMENT,
   `user_id` int(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `number` varchar(12) NOT NULL,
-  `message` varchar(500) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+  `message` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_message_users` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `orders` (
-  `id` int(100) NOT NULL,
+  `id` int(100) NOT NULL AUTO_INCREMENT,
   `user_id` int(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `number` varchar(12) NOT NULL,
@@ -36,77 +38,32 @@ CREATE TABLE `orders` (
   `total_products` varchar(1000) NOT NULL,
   `total_price` int(100) NOT NULL,
   `placed_on` varchar(50) NOT NULL,
-  `payment_status` varchar(20) NOT NULL DEFAULT 'pending'
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+  `payment_status` varchar(20) NOT NULL DEFAULT 'pending',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_orders_users` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `products` (
-  `id` int(100) NOT NULL,
+  `id` int(100) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `price` int(100) NOT NULL,
   `author` varchar(100) NOT NULL,
   `image` varchar(100) NOT NULL,
-  `quantity` int(100) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+  `quantity` int(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `users` (
-  `id` int(100) NOT NULL,
+  `id` int(100) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `user_type` varchar(20) NOT NULL DEFAULT 'user'
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+  `user_type` varchar(20) NOT NULL DEFAULT 'user',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE
-  `cart`
-ADD
-  PRIMARY KEY (`id`);
+ALTER TABLE `cart` ADD CONSTRAINT `fk_cart_products` FOREIGN KEY (`name`) REFERENCES `products`(`name`) ON DELETE CASCADE;
 
-ALTER TABLE
-  `message`
-ADD
-  PRIMARY KEY (`id`);
-
-ALTER TABLE
-  `orders`
-ADD
-  PRIMARY KEY (`id`);
-
-ALTER TABLE
-  `products`
-ADD
-  PRIMARY KEY (`id`);
-
-ALTER TABLE
-  `users`
-ADD
-  PRIMARY KEY (`id`);
-
-ALTER TABLE
-  `cart`
-MODIFY
-  `id` int(100) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 57;
-
-ALTER TABLE
-  `message`
-MODIFY
-  `id` int(100) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 10;
-
-ALTER TABLE
-  `orders`
-MODIFY
-  `id` int(100) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 10;
-
-ALTER TABLE
-  `products`
-MODIFY
-  `id` int(100) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE
-  `users`
-MODIFY
-  `id` int(100) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `orders` ADD CONSTRAINT `fk_orders_products` FOREIGN KEY (`total_products`) REFERENCES `products`(`name`) ON DELETE CASCADE;
 
 COMMIT;
